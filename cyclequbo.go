d98e02e1324be03ac9118ccd07d3eca1fb8def04
PJ v M <pjvm742@disroot.org>
2022-07-02 16:27:05 +0000

    final version; cyclequbo: rings, show breaks
    allow gradual increase/decrease of penalty multiplier between
    different rings;
    if no feasible solutions found, show the breaks

--- cyclequbo.go
+++ cyclequbo.go
@@ -27,7 +27,7 @@ type translentry struct {
 type transltable []translentry
 
 
-func constructqubo(g cwdgraph, penmult float64, vdis bool, adis bool) (qubo, transltable) {
+func constructqubo(g cwdgraph, penmult float64, vdis bool, adis bool, rings []int, ringfactor float64) (qubo, transltable) {
 	a := g.arcc
 	n := len(a)
 
@@ -150,6 +150,7 @@ func constructqubo(g cwdgraph, penmult float64, vdis bool, adis bool) (qubo, tra
 
 	/* now the penalties */
 	for i := 0; i < n; i++ {
+		localmult := penmult * math.Pow(ringfactor, float64(rings[i]))
 		var indices []int
 		var values []int
 		for k := voffsets[i]; k < len(vartable); k++ {
@@ -173,9 +174,10 @@ func constructqubo(g cwdgraph, penmult float64, vdis bool, adis bool) (qubo, tra
 				values = append(values, - abit.bitval)
 			}
 		}
-		addquadpen(QUBO, indices, values, penmult)
+		addquadpen(QUBO, indices, values, localmult)
 	}
 	for j := 0; j < n; j++ {
+		localmult := penmult * math.Pow(ringfactor, float64(rings[j]))
 		var indices []int
 		var values []int
 		for k := voffsets[j]; k < len(vartable); k++ {
@@ -190,7 +192,6 @@ func constructqubo(g cwdgraph, penmult float64, vdis bool, adis bool) (qubo, tra
 			if a[i][j] == 0 {
 				continue
 			}
-			// fmt.Println("Hello")
 			for k := aoffsets[i][j]; k < len(vartable); k++ {
 				abit := vartable[k]
 				if abit.start != i || abit.end != j {
@@ -200,7 +201,7 @@ func constructqubo(g cwdgraph, penmult float64, vdis bool, adis bool) (qubo, tra
 				values = append(values, - abit.bitval)
 			}
 		}
-		addquadpen(QUBO, indices, values, penmult)
+		addquadpen(QUBO, indices, values, localmult)
 	}
 
 	return QUBO, vartable
@@ -226,6 +227,56 @@ func addquadpen(qubomatrix qubo, indices []int, values []int, mult float64) {
 			qubomatrix[i][j] += mult * float64(val1 * val2)
 		}
 	}
+}
+
+func mkrings(arcs capmat) ([]int, int) {
+	n := len(arcs)
+	rings := make([]int, n)
+	rings[0] = 1
+
+	r := 1
+	full := false
+	for !full {
+		done := true
+		for i:= 0; i < n; i++ {
+			if rings[i] == r {
+				for j := 0; j < n; j++ {
+					if rings[j] == 0 && ( arcs[i][j] > 0 || arcs[j][i] > 0 ) {
+						rings[j] = r + 1
+						done = false
+					}
+				}
+			}
+		}
+
+		if done {
+			for i := 0; i < n; i++ {
+				if rings[i] == 0 {
+					rings[i] = 1
+					break
+				}
+			}
+			r = 1
+		} else {
+			r++
+		}
+
+		full = true
+		for i := 0; i < n; i++ {
+			if rings[i] == 0 {
+				full = false
+			}
+		}
+	}
+
+	max := 0
+	for i := 0; i < n; i++ {
+		if rings[i] > max {
+			max = rings[i]
+		}
+	}
+
+	return rings, max
 }
 
 func simplify(g cwdgraph) (cwdgraph, []int) {
@@ -488,6 +539,8 @@ func main() {
 	var penmult float64 = 1
 	var absmult bool
 	var expectmult bool
+	var ringf float64 = 1
+	var expectringf bool
 	var adis bool
 	var vdis bool
 	for a := 1; a < nargs; a++ {
@@ -498,6 +551,13 @@ func main() {
 				os.Exit(1)
 			}
 			expectmult = false
+		} else if expectringf {
+			_, err := fmt.Sscanf(args[a], "%f", &ringf)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Malformed arguments: argument after -r isn't a number")
+				os.Exit(1)
+			}
+			expectringf = false
 		} else if args[a] == "-a" {
 			adis = true
 		} else if args[a] == "-v" {
@@ -508,6 +568,8 @@ func main() {
 		} else if args[a] == "-M" {
 			absmult = true
 			expectmult = true
+		} else if args[a] == "-r" {
+			expectringf = true
 		} else {
 			filename = args[a]
 		}
@@ -527,10 +589,13 @@ func main() {
 			}
 		}
 		avg := adjusted_avg(weights)
-		fmt.Fprintln(os.Stdout, avg)
+		fmt.Fprintf(os.Stderr, "Adjusted average  of the weights (scale for the penalties): %.2g\n", avg)
 		penmult *= avg
 	}
-	qubomatrix, tlt := constructqubo(reducedg, penmult, vdis, adis)
+	rings, max := mkrings(reducedg.arcc)
+	ringfactor := math.Pow(ringf, 1/float64(max))
+	penmult *= 1/ringfactor
+	qubomatrix, tlt := constructqubo(reducedg, penmult, vdis, adis, rings, ringfactor)
 	outputfile := strings.Replace(filename, ".graph.tsv", ".qubo.tsv", 1)
 	writeQUBO(outputfile, qubomatrix)
 	fmt.Fprintln(os.Stderr, "Please press enter when the solution file is there")
@@ -551,8 +616,10 @@ func processsolutions(filename string, tlt transltable, oldinds []int, weights w
 	r := csv.NewReader(f)
 	r.Comma = '\t'
 
+	s := 0
+	var firstsol []bool
 	row, err := r.Read()
-	for ; err == nil; row, err = r.Read() {
+	for first := true; err == nil; row, err = r.Read() {
 		sol := make([]bool, nvar)
 		for i := 0; i < nvar; i++ {
 			if row[i] == "0" {
@@ -564,19 +631,27 @@ func processsolutions(filename string, tlt transltable, oldinds []int, weights w
 				os.Exit(1)
 			}
 		}
+		if first {
+			firstsol = sol
+			first = false
+		}
 		vertflows, arcflows := backtranslate(sol, tlt, n)
 		if isfeasible(vertflows, arcflows) {
+			fmt.Fprintf(os.Stderr, "The %d-th solution is feasible\n", s)
 			fmt.Printf("Solution value: %d\n", solval(arcflows, weights))
 			printdecomp(arcflows, oldinds)
 			os.Exit(0)
 		}
-
+		s++
 	}
 	if err != io.EOF {
 		fmt.Fprintln(os.Stderr, "Something went wrong trying to read the solution file")
 		os.Exit(1)
 	}
 	fmt.Fprintln(os.Stderr, "None of the solutions are feasible")
+	fmt.Fprintln(os.Stderr, "Breaks in the first solution:")
+	vertflows, arcflows := backtranslate(firstsol, tlt, n)
+	showbreaks(vertflows, arcflows, oldinds)
 }
 
 func readgraph(filename string) cwdgraph {
@@ -657,7 +732,7 @@ func writeQUBO(filename string, problem qubo) {
 	for i := 0; i < n; i++ {
 		row := make([]string, n)
 		for j := 0; j < n; j++ {
-			row[j] = fmt.Sprintf("%.4g", problem[i][j])
+			row[j] = fmt.Sprintf("%.6g", problem[i][j])
 		}
 		rep[i] = row
 	}
@@ -671,5 +746,24 @@ func showmat(mat [][]int) {
 			fmt.Printf("%d  ", mat[i][j])
 		}
 		fmt.Printf("\n")
+	}
+}
+
+func showbreaks(vertices []int, arcs capmat, oldinds []int) {
+	n := len(vertices)
+	for i := 0; i < n; i++ {
+		flow := vertices[i]
+		sum1 := 0
+		sum2 := 0
+		for j := 0; j < n; j++ {
+			sum1 += arcs[i][j]
+			sum2 += arcs[j][i]
+		}
+		if sum1 != flow || sum2 != flow {
+			fmt.Fprintf(os.Stderr, "Break at vertex %d; %d in simplified graph\n", oldinds[i], i)
+			fmt.Fprintf(os.Stderr, "In: %d\n", sum2)
+			fmt.Fprintf(os.Stderr, "Trough: %d\n", flow)
+			fmt.Fprintf(os.Stderr, "Out: %d\n", sum1)
+		}
 	}
 }
